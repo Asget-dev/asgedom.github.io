80660d9a1aeea48c037d47bcc4240086c274510b
asgdomhail@gmail.com <63181283+Asget-dev@users.noreply.github.com>
2021-03-25 11:50:54 -0700

    Update app.js

--- labW1D3/app.js
+++ labW1D3/app.js
@@ -13,7 +13,25 @@ Creates an object that contains the following information from the "this" object
 }	
 */
 function analyzer() {
-	return {};
+  let count = 0;
+  for (let pro in this) {
+    count++;
+  }
+
+  let countTwo = 0;
+  for (let key in this) {
+    if (key.length < 3) {
+      countTwo++;
+    }
+  }
+  let countThree = 0;
+  for (let key in this) {
+    if (typeof this[key] === "object") {
+      countThree++;
+    }
+  }
+
+  return { numProperties: count, cntShortName: countTwo, cntReference: countThree };
 }
 
 /* Constructor for a person object
@@ -26,18 +44,19 @@ function analyzer() {
 	}
 */
 function Person(name, country, grades) {
-	this.name = name;
-	this.country = country;
-	this.grades = grades;
+  this.name = name;
+  this.country = country;
+  this.grades = grades;
+  // computeGrade;
 
-	this.computeGrade = function(grades){
-		let sum=0;
-		let result;
-		for(let i=0; i < this.grades.length; i++){
-			sum+=this.grades[i];
-		}
-		result = sum/this.grades.length;
-		return result
-	};
-};
-let person = new Person("bob","usa",computeGrade());
+  this.computeGrade = function (grades) {
+    let sum = 0;
+    let result;
+    for (let i = 0; i < this.grades.length; i++) {
+      sum += this.grades[i];
+    }
+    result = sum / this.grades.length;
+    return result;
+  };
+}
+let person = new Person("bob", "usa", computeGrade());
